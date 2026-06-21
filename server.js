0e2ec1ca411d0cf07d83aaf1ba056c7e9441215f
Daniel Silva <daniel.das.itapeva2015@gmail.com>
2026-06-21 12:02:03 -0300

    Adicionando segurança SSL para o banco de dados

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('error', (err) => {
